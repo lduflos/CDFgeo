20c6555de3b40b9665dfbc9a7fc1976385f1f581
Thibault Clérice <1929830+PonteIneptique@users.noreply.github.com>
2018-06-15 09:54:22 +0200

    Correction des Relationships et ForeignKeys

--- cdfgeo/modeles/donnees.py
+++ cdfgeo/modeles/donnees.py
@@ -14,8 +14,8 @@ class Chaire(db.Model):
     chaire_debut = db.Column(db.String(4))
     chaire_fin = db.Column(db.String(4))
     chaire_precision = db.Column(db.Text)
-    '''# Jointure
-    chaire_personne = db.relationship("Personne", uselist=False, back_populates="personne_chaire")'''
+    # Jointure
+    personnes = db.relationship("Personne", uselist=False, back_populates="chaire")
 
 
 ##############################################################################################################
@@ -33,9 +33,9 @@ class Personne(db.Model):
     personne_precision = db.Column(db.Text)
     personne_liensexternes = db.Column(db.Text)
     chaire_chaire_id = db.Column(db.Integer, db.ForeignKey('chaire.chaire_id'))
-    '''# Jointures
-    personne_chaire = db.relationship("Chaire", back_populates="chaire_personne")
-    personne_mission = db.relationship("Personne_Mission", back_populates="lien_personne_mission")'''
+    # Jointures
+    chaire = db.relationship("Chaire", back_populates="personnes")
+    missions = db.relationship("Personne_Mission", back_populates="personne")
 
 
 ##############################################################################################################
@@ -48,8 +48,8 @@ class Ville(db.Model):
     ville_intitule = db.Column(db.Text)
     ville_lat = db.Column(db.Float)
     ville_long = db.Column(db.Float)
-    '''# Jointure
-    lieu_mission = db.relationship("Mission_Lieu", back_populates="lien_lieu_mission_ville")'''
+    # Jointure
+    missions = db.relationship("Mission_Lieu", back_populates="ville")
 
 
 ##############################################################################################################
@@ -62,8 +62,8 @@ class Pays(db.Model):
     pays_intitule = db.Column(db.Text)
     pays_lat = db.Column(db.Float)
     pays_long = db.Column(db.Float)
-    '''# Jointure
-    lieu_mission = db.relationship("Mission_Lieu", back_populates="lien_lieu_mission_pays")'''
+    # Jointure
+    missions = db.relationship("Mission_Lieu", back_populates="pays")
 
     def pays_to_json(self):
         return {
@@ -72,12 +72,18 @@ class Pays(db.Model):
             "attributes": {
                 "nom": self.pays_intitule,
                 "longitude": self.pays_long,
-                "latitude": self.pays_lat}
+                "latitude": self.pays_lat
+            },
+            "mission": [
+                lieu_mission.mission.mission_intitule
+                for lieu_mission in self.missions
+            ]
         }
 
 ##############################################################################################################
 #                                                   MISSION                                                  #
 ##############################################################################################################
+
 
 class Mission(db.Model):
     __tablename__ = "mission"
@@ -89,10 +95,9 @@ class Mission(db.Model):
     mission_date_fin = db.Column(db.String(10))
     mission_dates = db.Column(db.Text)
     mission_precision = db.Column(db.Text)
-    '''# Jointures
-    mission_personne = db.relationship("Personne_Mission", back_populates="lien_mission_personne")
-    mission_lieu1 = db.relationship("Mission_Lieu", back_populates="lien_mission_lieu_ville")
-    mission_lieu2 = db.relationship("Mission_Lieu", back_populates="lien_mission_lieu_pays")'''
+    # Jointures
+    personnes = db.relationship("Personne_Mission", back_populates="mission")
+    lieux = db.relationship("Mission_Lieu", back_populates="mission")
 
 
 ##############################################################################################################
@@ -101,11 +106,11 @@ class Mission(db.Model):
 class Personne_Mission(db.Model):
     __tablename__ = "personne_mission"
     personne_mission_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
-    personne_mission_personne_id = db.Column(db.Integer, db.ForeignKey('mission.personne_id'))
-    personne_mission_mission_id = db.Column(db.Integer, db.ForeignKey('personne.mission_id'))
-    '''# Jointures
-    lien_personne_mission = db.relationship("Personne", back_populates="personne_mission")
-    lien_mission_personne = db.relationship("Mission", back_populates="mission_personne")'''
+    personne_mission_personne_id = db.Column(db.Integer, db.ForeignKey('personne.personne_id'))
+    personne_mission_mission_id = db.Column(db.Integer, db.ForeignKey('mission.mission_id'))
+    # Jointures
+    personne = db.relationship("Personne", back_populates="missions")
+    mission = db.relationship("Mission", back_populates="personnes")
 
 
 ##############################################################################################################
@@ -114,10 +119,10 @@ class Personne_Mission(db.Model):
 class Mission_Lieu(db.Model):
     __tablename__ = "mission_lieu"
     mission_lieu_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
-    mission_lieu_mission_id = db.Column(db.Integer, db.ForeignKey('lieu.mission_id'))
-    mission_lieu_ville_id = db.Column(db.Integer, db.ForeignKey('lieu.ville_id'))
-    mission_lieu_pays_id = db.Column(db.Integer, db.ForeignKey('lieu.pays_id'))
-    '''# Jointures
-    lien_lieu_mission_ville = db.relationship("Ville", back_populates="lieu_mission1")
-    lien_lieu_mission_pays = db.relationship("Pays", back_populates="lieu_mission2")
-    lien_mission_lieu = db.relationship("Mission", back_populates="mission_personne")'''
+    mission_lieu_mission_id = db.Column(db.Integer, db.ForeignKey('mission.mission_id'))
+    mission_lieu_ville_id = db.Column(db.Integer, db.ForeignKey('ville.ville_id'))
+    mission_lieu_pays_id = db.Column(db.Integer, db.ForeignKey('pays.pays_id'))
+    # Jointures
+    ville = db.relationship("Ville", back_populates="missions")
+    pays = db.relationship("Pays", back_populates="missions")
+    mission = db.relationship("Mission", back_populates="lieux")
