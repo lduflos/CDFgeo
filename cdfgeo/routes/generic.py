c3de488a6998c3895577f5f17483556dced8b427
lduflos <duflos.lea@enc-sorbonne.fr>
2018-06-27 12:44:39 +0200

    ajout route pour affichage json pays - propre

--- cdfgeo/routes/generic.py
+++ cdfgeo/routes/generic.py
@@ -1,6 +1,6 @@
 from flask import render_template, jsonify, request
 from ..app import app
-from ..modeles.donnees import Mission
+from ..modeles.donnees import Mission, Pays
 #############################################################################
 #                             PAGES ACCUEIL                                  #
 #############################################################################
@@ -12,14 +12,14 @@ def accueil():
 
 @app.route("/a-propos")
 def a_propos():
-    """ Route pour affcher les informations générale sur l'application
+    """ Route pour afficher les informations générales sur l'application
     """
     return render_template("pages/a_propos.html")
   
 @app.route("/test_affichage")
 def missions():
+    """ Route qui récupère les missions et les affiche sur une carte
     """
-    :return """
 
     missions = Mission.query.all()
 
@@ -27,9 +27,14 @@ def missions():
     for mission in missions:
         lieu=mission.missions(data)
 
-    print(lieu)
-
     return render_template("test_affichage.html", lieu=lieu)
+
+@app.route("/pays/<pays_id>")
+def _recup_json(pays_id):
+    """ Route qui récupère les données des pays en json
+    """
+    pays = Pays.query.get_or_404(pays_id)
+    return jsonify(pays.pays_to_json())
 
 '''
 #############################################################################
